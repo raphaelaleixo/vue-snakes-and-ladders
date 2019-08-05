756b24c0b1386488465aa9a0b110d9fe94d81834
Raphael Aleixo <raleixo@lumis.com.br>
2019-08-05 13:58:32 -0300

    Finishing board design

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,59 @@
 <template>
   <div id="app">
-    <transition-group name="list-complete"
+    <transition-group name="move-piece"
       tag="div"
-      class="grid"
+      class="board"
       :style="{'grid-template-areas':boardGrid}">
-      <div class="grid__item square"
+      <div class="board__square square"
         :class="{'square--higher':square.type === 'higher','square--lower':square.type==='lower' }"
         :style="{'grid-area':'square'+(square.number)}"
-        v-for="(square) in gameBoard"
+        v-for="(square) in board.squares"
         :key="square.number">
-        {{square.number}}
+        <span class="square__number">{{square.number}}</span>
         <span v-if="square.to"
-          class="square__to">{{square.to}}</span>
+          class="square__number square__to">{{square.to}}</span>
       </div>
       <div class="piece"
-        @click="walkTo(3)"
+        @click="walkTo($event)"
         key="piece1"
-        :style="{'grid-area':'square'+position}"></div>
+        :style="{'grid-area':'square'+position, 'pointer-events:none' : walked !== 0}"></div>
     </transition-group>
   </div>
 </template>
 
 <script>
-  let timeout;
+  let el;
   export default {
     name: "app",
     data () {
       return {
-        gameBoard: [],
-        board: 4,
-        position: 1,
+        board: {
+          base: 8,
+          squares:[],
+          specialSquares:[1],
+          tries:0
+        },
+        position: 60,
         walked: 0,
-        tries: 0,
-        specialSquares: [],
         dice1: 1,
         dice2: 1
       }
     },
     computed: {
-      boardSquares () {
-        return this.board * this.board;
+      totalSquares () {
+        return this.board.base * this.board.base;
       },
       boardGrid () {
         let grid = "";
-        for (let i = this.board - 1; i >= 0; i--) {
+        for (let i = this.board.base - 1; i >= 0; i--) {
           let boardRow = '"';
           if (i % 2 === 0) {
-            for (let k = i * this.board + 1; k < this.board * i + this.board + 1; k++) {
+            for (let k = i * this.board.base + 1; k < this.board.base * i + this.board.base + 1; k++) {
               boardRow += " square" + k;
             }
             boardRow += '"';
           } else {
-            for (let k = this.board * i + this.board; k > i * this.board; k--) {
+            for (let k = this.board.base * i + this.board.base; k > i * this.board.base; k--) {
               boardRow += " square" + k;
             }
             boardRow += '"';
@@ -66,70 +68,80 @@
         this.dice1 = Math.floor(Math.random() * 6) + 1;
         this.dice2 = Math.floor(Math.random() * 6) + 1;
       },
-      walkTo () {
+      walkTo (event) {
         this.rollDice();
         this.walked = this.dice1 + this.dice2;
-        this.trywalk()
+        this.trywalk(event.target)
       },
       trywalk () {
-        clearTimeout(timeout)
-        if (this.walked > 0) {
-          this.position++;
-          if (this.position > this.boardSquares) {
-            this.position--;
-            this.bounceBack()
+        el.removeEventListener('animationend', walk)
+        const walk = () => {
+          if (this.walked > 0) {
+            this.position++;
+            if (this.position > this.totalSquares) {
+              this.position--;
+              this.bounceBack()
+              el.removeEventListener('animationend', walk)
+            } else {
+              this.walked--;
+              el.addEventListener('animationend', walk)
+            }
           } else {
-            this.walked--;
-            timeout = setTimeout(() => this.trywalk(), 300);
+            this.checkPosition()
+            el.removeEventListener('animationend', walk)
           }
-        } else {
-          this.checkPosition()
         }
+        walk();
       },
       bounceBack () {
-        clearTimeout(timeout)
-        if (this.walked > 0) {
-          this.position--;
-          this.walked--;
-          timeout = setTimeout(() => this.bounceBack(), 300);
-        } else {
-          this.checkPosition()
+        el.removeEventListener('animationend', bounce)
+        const bounce = () => {
+          if (this.walked > 0) {
+            this.position--;
+            this.walked--;
+            el.addEventListener('animationend', bounce)
+          } else {
+            this.checkPosition()
+            el.removeEventListener('animationend', bounce)
+          }
         }
+        bounce()
       },
       checkPosition () {
-        let specialPositions = this.gameBoard.map(item => item.from);
+        let specialPositions = this.board.squares.map(item => item.from);
         if (specialPositions.includes(this.position)) {
-          let rule = this.gameBoard.filter(item => item.from === this.position)
+          let rule = this.board.squares.filter(item => item.from === this.position)
           this.position = rule[0].to
         }
       },
       chooseRandomSquare (higherOrLower, numberToCompare) {
         this.tries++;
+        let specialSquare = Math.floor(Math.random() * (this.totalSquares)) + 1;
+       
         if (numberToCompare === 'error' || this.tries > 10) {
           this.tries = 0;
           return 'error';
         }
-        let specialSquare = Math.floor(Math.random() * (this.boardSquares - 3)) + 2;
-        if ((higherOrLower === 'higher' && specialSquare < numberToCompare) || this.specialSquares.includes(specialSquare)) {
-          return this.chooseRandomSquare(higherOrLower, numberToCompare)
-        }
-        else if ((higherOrLower === 'lower' && specialSquare > numberToCompare) || this.specialSquares.includes(specialSquare)) {
-          return this.chooseRandomSquare(higherOrLower, numberToCompare)
-        }
-        else if (this.specialSquares.includes(specialSquare)) {
+        
+        if (this.board.specialSquares.includes(specialSquare)) {
           return this.chooseRandomSquare(higherOrLower, numberToCompare);
         }
-        else {
-          this.specialSquares = [specialSquare, ...this.specialSquares]
+        
+        if (higherOrLower === 'higher' && specialSquare > numberToCompare ||
+            higherOrLower === 'lower' && specialSquare < numberToCompare ||
+            !higherOrLower) {
           return specialSquare;
+        } else {
+           return this.chooseRandomSquare(higherOrLower, numberToCompare);
         }
       },
       createGameBoard () {
-        for (let i = 1; i < this.boardSquares + 1; i++) {
-          this.gameBoard = [...this.gameBoard, {
+        for (let i = 1; i < this.totalSquares + 1; i++) {
+          this.board.squares = [...this.board.squares, {
             number: i
           }]
         }
+        this.board.specialSquares = [...this.board.specialSquares, this.totalSquares]
       },
       createSpecialSquare (higherOrLower) {
         let firstNumber = this.chooseRandomSquare();
@@ -138,13 +150,15 @@
           this.createSpecialSquare(higherOrLower);
         }
         else {
-          this.gameBoard = this.gameBoard.map(item => item.number === firstNumber ? { ...item, from: firstNumber, to: secondNumber, type: higherOrLower } : item)
+          this.board.specialSquares = [firstNumber, secondNumber, ...this.board.specialSquares]
+          this.board.squares = this.board.squares.map(item => item.number === firstNumber ? { ...item, from: firstNumber, to: secondNumber, type: higherOrLower } : item)
         }
       },
     },
     mounted () {
+      el = document.querySelector('.piece');
       this.createGameBoard();
-      for (let i = 0; i < Math.floor(this.board / 2); i++) {
+      for (let i = 0; i < Math.floor(this.board.base / 2); i++) {
         this.createSpecialSquare('higher');
         this.createSpecialSquare('lower');
       }
@@ -156,7 +170,7 @@
   * {
     box-sizing: border-box;
   }
-  .grid {
+  .board {
     height: 30em;
     width: 30em;
     display: grid;
@@ -169,15 +183,16 @@
     overflow: hidden;
   }
 
-  .grid__item {
+  .board__square {
     background: #ffffcf;
     padding: 0.5em;
     font-size: 0.7em;
     font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
     font-weight: bolder;
+    position: relative;
   }
 
-  .grid__item:nth-child(even) {
+  .board__square:nth-child(even) {
     background: #e3e3b6;
   }
 
@@ -197,10 +212,13 @@
     background: #f3abb6;
   }
 
-  .square__to {
+  .square__number {
     position: absolute;
+  }
+  
+  .square__to { 
     color: green;
-    display: block;
+    bottom:0.5em;
   }
 
   .square__to:after {
@@ -235,12 +253,7 @@
     box-shadow: inset 0 0 0 2px darkred, 0 0 0.5em 0 #00000088;
   }
 
-  .list-complete-leave-active {
-    position: absolute;
-    pointer-events: none;
-  }
-
-  .list-complete-move:before {
+  .move-piece-move:before {
     animation: walk 0.3s linear 0s;
   }
 
